4b58ef513568bd6a3adffe6e731ed2a0aaa27449
Kent Andersen <kentareandersen@gmail.com>
2016-07-24 00:52:55 +0200

    added scaffolding for react with babel

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+const src = path.resolve(__dirname, 'src/public');
+const dist = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: path.join(src, 'main.js'),
+  output: {
+    path: dist,
+    filename: 'board.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test : /\.jsx?/,
+      include : src,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }]
+  }
+};
